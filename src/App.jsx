68beaf5b8edc015bf1f860cc02dc844c2525c0e1
onifs10 <onifadebolu64@gmail.com>
2022-02-19 23:21:50 +0100

    ui completed

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+        <div className='title'>Tic-Tac-To</div>
+        <TickTack/>
+    </div>
+  )
+}
+
+const TickTack = () => {
+  const [player, setPlayer] = useState(1);
+
+  const played = () => {
+    if (player === 1) {
+      setPlayer(2)
+    } else {
+      setPlayer(1)
+    }
+  }
+  return <div className='tic_tac'>
+    <Square player={player} played={played} id={1}/>
+    <Square player={player} played={played} id={2}/>
+    <Square player={player} played={played} id={3}/>
+    <Square player={player} played={played} id={4}/>
+    <Square player={player} played={played} id={5}/>
+    <Square player={player} played={played} id={6}/>
+    <Square player={player} played={played} id={7}/>
+    <Square player={player} played={played} id={8}/>
+    <Square player={player} played={played} id={9}/>
+  </div>
+}
+
+const Square = ({id, player, played}) => {
+  const [tick, setTick] = useState('')
+  const [ticked, setTicked] = useState(false);
+
+  return (
+    <button className={`square ${tick}` }onClick={() => {
+      if (ticked) {
+        return
+      }
+      if (player == 1) {
+        setTick("X")     
+      } else {
+        setTick("O")
+      }
+      setTicked(v => !v);
+      played?.()
+    }}>
+      {ticked && tick}
+    </button>
+  )
+}
+
+export default App
+
+
